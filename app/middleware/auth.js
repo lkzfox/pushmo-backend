0062023376e4692c7aed96ae2d1ad837637c3e32
Lucas Feitoza <jlkzfp@gmail.com>
2019-09-24 21:41:31 -0300

    Removed image requested endpoint from authentication.

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -3,6 +3,11 @@ const config = require('config');
 
 module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
+
+    if ((/api.{1}pressure_ulcer.{1}image/).test(req.url)) {
+        return next();
+    }
+    
     if (!token) return res.status(400).send('No token provided');
     try {
         const validate = jwt.verify(token, config.get('jwtKey'));
